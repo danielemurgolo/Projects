19022b03793fa29a04dfad089070873728a7d49d
danielemurgolo <daniele.murgolo@protonmail.com>
2023-03-26 21:52:27 +0200

    added plots

--- Loan/loan.py
+++ Loan/loan.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy_financial as npf
+import matplotlib.pyplot as plt
 
 loan = 60_000
 rate = 0.04
@@ -9,4 +10,7 @@ per = np.arange(years * 12) + 1
 ipmt = -npf.ipmt(rate / 12, per, years * 12, loan)
 ppmt = -npf.ppmt(rate / 12, per, years * 12, loan)
 
-print(ipmt)
+plt.plot(per, ipmt, label="interests")
+plt.plot(per, ppmt, label="principal")
+plt.legend()
+plt.show()
